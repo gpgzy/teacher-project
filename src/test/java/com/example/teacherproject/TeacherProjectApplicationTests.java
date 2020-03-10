72050fe9d5339d1b5cb0fc44ba232d2d9675fb15
gzy <786133379@qq.com>
2020-03-10 15:44:57 +0800

    初始化

---
+++ src/test/java/com/example/teacherproject/TeacherProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
